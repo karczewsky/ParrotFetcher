56b02f91a6712845fdc47bdf1d892949199b8566
wolpear <wolpear@users.noreply.github.com>
2018-11-27 12:06:07 +0100

    Worker pool

--- main.go
+++ main.go
@@ -15,7 +15,6 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"time"
 
 	yaml "gopkg.in/yaml.v2"
 )
@@ -32,8 +31,48 @@ type Emoji struct {
 }
 
 const (
-	ParrotDirectory = "./img/"
+	parrotDirectory = "./img/"
+	workerCount     = 5
 )
+
+func worker(jobs chan Emoji, done chan string) {
+	fmt.Println("Bringing up worker...")
+	for j := range jobs {
+		fetchEmote(j, done)
+	}
+}
+
+func fetchEmote(emoji Emoji, done chan string) {
+	fmt.Printf("Fetching %v...\n", emoji.Fullname)
+
+	resp, err := http.Get(emoji.Src)
+	if resp.StatusCode < 200 || resp.StatusCode >= 300 {
+		done <- fmt.Sprintf("Error fetching %v CODE: %v URL: %v ", emoji.Name, resp.StatusCode, emoji.Src)
+		return
+	}
+	if err != nil {
+		done <- fmt.Sprintf("Error fetching %v", emoji.Name)
+		return
+	}
+
+	f, err := os.Create(parrotDirectory + emoji.Name + ".gif")
+	if err != nil {
+		done <- fmt.Sprintf("Error creating file for %v", emoji.Name)
+		return
+	}
+	defer f.Close()
+
+	b := make([]byte, 8)
+
+	for {
+		n, err := resp.Body.Read(b)
+		f.Write(b[:n])
+		if err == io.EOF {
+			break
+		}
+	}
+	done <- fmt.Sprintf("Fetched %v", emoji.Fullname)
+}
 
 func main() {
 	parrotData := ParrotsData{}
@@ -41,10 +80,10 @@ func main() {
 	if len(os.Args) < 2 {
 		log.Fatal("Please point parrot file as command argument")
 	}
+
 	parrotFile := os.Args[1]
 
 	data, err := ioutil.ReadFile(parrotFile)
-
 	if err != nil {
 		log.Fatal(fmt.Sprintf("Error reading file %v \nError: %v", parrotFile, err))
 	}
@@ -54,45 +93,19 @@ func main() {
 		log.Fatal(fmt.Sprintf("Error unmarshaling file %v \nError: %v", parrotFile, err))
 	}
 
-	if _, err := os.Stat(ParrotDirectory); os.IsNotExist(err) {
-		os.Mkdir(ParrotDirectory, 0755)
+	if _, err := os.Stat(parrotDirectory); os.IsNotExist(err) {
+		os.Mkdir(parrotDirectory, 0755)
 	}
 
-	done := make(chan string)
+	jobs := make(chan Emoji, 200)
+	done := make(chan string, 200)
 
-	for _, emoji := range parrotData.Emojis {
-		time.Sleep(35 * time.Millisecond)
-		go func(emoji Emoji, done chan string) {
-			fmt.Printf("Fetching %v...\n", emoji.Fullname)
+	for i := 0; i < workerCount; i++ {
+		go worker(jobs, done)
+	}
 
-			resp, err := http.Get(emoji.Src)
-			if resp.StatusCode < 200 || resp.StatusCode >= 300 {
-				done <- fmt.Sprintf("Error fetching %v CODE: %v URL: %v ", emoji.Name, resp.StatusCode, emoji.Src)
-				return
-			}
-			if err != nil {
-				done <- fmt.Sprintf("Error fetching %v", emoji.Name)
-				return
-			}
-
-			f, err := os.Create(ParrotDirectory + emoji.Name + ".gif")
-			if err != nil {
-				done <- fmt.Sprintf("Error creating file for %v", emoji.Name)
-				return
-			}
-			defer f.Close()
-
-			b := make([]byte, 8)
-
-			for {
-				n, err := resp.Body.Read(b)
-				f.Write(b[:n])
-				if err == io.EOF {
-					break
-				}
-			}
-			done <- fmt.Sprintf("Fetched %v", emoji.Fullname)
-		}(emoji, done)
+	for _, e := range parrotData.Emojis {
+		jobs <- e
 	}
 
 	for i := 0; i < len(parrotData.Emojis); i++ {
--- server.go
+++ server.go
@@ -12,16 +12,17 @@ const (
 )
 
 func runServer() {
-	fs := http.FileServer(http.Dir(ParrotDirectory))
+	fs := http.FileServer(http.Dir(parrotDirectory))
 	http.Handle("/static/", http.StripPrefix("/static", fs))
 	http.HandleFunc("/", parrotViewHandler)
+	fmt.Printf("Starting server on http://localhost:8080")
 	log.Fatal(http.ListenAndServe(port, nil))
 
 }
 
 func parrotViewHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>Parrot list</h1>")
-	files, err := ioutil.ReadDir(ParrotDirectory)
+	files, err := ioutil.ReadDir(parrotDirectory)
 	if err != nil {
 		log.Fatal(err)
 	}
